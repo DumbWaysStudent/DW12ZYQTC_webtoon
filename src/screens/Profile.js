d26c55567cc066d0dafda8826f2e3997ad046e0f
Arfifa Rahman <arfifarahman509@gmail.com>
2019-10-13 18:30:37 +0700

    add profile screen

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,45 +1,72 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
-import {
-    Container,
-    Text,
-    View,
-} from 'native-base';
+import { Text, View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-class Profile extends Component {
+export class Profile extends Component {
     render() {
         return (
-            <Container style={styles.container}>
-                <View style={styles.viewContent}>
-                    <Text style={styles.textTitle}>Comic Paper</Text>
-                    <Text style={styles.textSubTitle}>
-                        welcome happy reading
-                    </Text>
+            <View style={{ flex: 1 }}>
+                <View style={styles.header}>
+                    <Text style={{ fontSize: 15 }}>Profile</Text>
+                    <TouchableWithoutFeedback onPress={() => console.log('edit profile')}>
+                        <Icon name="pencil" size={25} color="#009b00" />
+                    </TouchableWithoutFeedback>
                 </View>
-            </Container>
+                <View style={styles.profile}>
+                    <View style={styles.profileImage}>
+                        <Icon name="user" color="#ccc" size={140} />
+                    </View>
+                    <Text style={{ fontSize: 23 }}>Your Name</Text>
+                </View>
+                <TouchableWithoutFeedback onPress={() => console.log('change screen')}>
+                    <View style={[styles.content, { flexDirection: 'row', justifyContent: 'space-between' }]}>
+                        <Text style={styles.textContent}>My Webtoon Creation</Text>
+                        <Icon name="chevron-right" color='#ccc' size={25} />
+                    </View>
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={() => console.log('logout')}>
+                    <Text style={[styles.textContent, { padding: 10, borderColor: '#d3d3d3', borderWidth: 1, }]}>Log Out</Text>
+                </TouchableWithoutFeedback>
+            </View>
         );
     }
 }
 
-export default Profile;
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#f5f6fa',
+    header: {
+        flexDirection: 'row',
+        height: 60,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+        elevation: 2,
+    },
+    profile: {
+        paddingVertical: 40,
         alignItems: 'center',
     },
-    viewContent: {
-        flex: 1,
+    profileImage: {
+        width: 150,
+        height: 150,
         justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 100,
+        borderWidth: 10,
+        borderColor: '#ccc',
+        marginBottom: 20,
     },
-    textTitle: {
-        fontSize: 40,
-        textAlign: 'center',
+    content: {
+        padding: 10,
+        borderColor: '#d3d3d3',
+        borderWidth: 1,
+        marginBottom: 1,
     },
-    textSubTitle: {
-        fontSize: 15,
-        marginBottom: '10%',
-        textAlign: 'center',
+    textContent: {
+        fontSize: 18,
     },
 });
+
+export default Profile;
